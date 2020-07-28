f55c3ce0f26d175351a00451267e84b5506146ea
shichenchong <shichenchong@inspur.com>
2020-07-28 15:35:13 +0800

    两数相加

---
+++ leetcode1/src/test/java/com/sceon/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sceon.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
